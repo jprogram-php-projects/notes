95a328ae852cb14286b5137aaaae11dfb320d091
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-09-25 14:56:47 -0300

    test modifying the routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    echo 'Hello World!';
+});
+
+Route::get('/about', function(){
+    echo 'About us';
 });
