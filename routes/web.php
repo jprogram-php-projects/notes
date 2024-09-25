635ee6a6a5c9566fab077b5f53e0a9377d156939
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-09-25 15:09:55 -0300

    Created a controller and a route for it

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,3 +14,5 @@ Route::get('/', function(){
 Route::get('/about', function(){
     echo 'About us';
 });
+
+Route::get('/main', [MainController::class, 'index']);
