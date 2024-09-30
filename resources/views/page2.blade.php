43b442232cc7ca71c1b46c159dd6a947c5ad566a
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-09-30 15:33:38 -0300

    Cleaning up the project

--- resources/views/page2.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main_layouts')
-
-@section('content')
-    <h1>Welcome View and Blade!</h1>
-    <hr>
-    <!-- <h3>The value is: <?= $value ?> </h3> -->
-    <h3>Page 2</h3>
-    <h3>The value is: {{ $value }} </h3>
-@endsection
