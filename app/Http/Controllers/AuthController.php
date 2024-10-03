cd982976608061a56fa4bb0d772205383192784d
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-10-03 14:19:00 -0300

    set custom texts for validation errors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,29 @@ class AuthController extends Controller
     public function loginSubmit(Request $request)
     {
         // dd($request);
-        echo $request->input('text_username'). '<br/>'. $request->input('text_password');
+        // echo $request->input('text_username'). '<br/>'. $request->input('text_password');
+
+        $request->validate(
+            // rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            // error messages
+            [
+                'text_username.required' => 'O username é obrigatório!',
+                'text_username.email'    => 'Username deve ser um email válido!',
+                'text_password.required' => 'A password é obrigatória!',
+                'text_password.min'      => 'A password deve ter pelo menos :min caracteres',
+                'text_password.max'      => 'A password deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
     }
 
     public function logout()
