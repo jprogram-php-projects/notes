44895727087d83e2f7dbc078c7d2d17f8b9a7220
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-10-21 16:09:18 -0300

    created the edit note view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Encryption\DecryptException;
-use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -25,13 +24,53 @@ class MainController extends Controller
 
     public function newNote()
     {
-        echo "I'm creating a new note!";
+        return view('new_note');
     }
 
+    public function newNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note'  => 'required|min:3|max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório!',
+                'text_title.min'      => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max'      => 'O título deve ter no máximo :max caracteres',
+                'text_note.required'  => 'A nota é obrigatória!',
+                'text_note.min'       => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max'       => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // get user id
+        $id = session('user.id');
+
+        // create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title   = $request->text_title;
+        $note->text    = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
+    }
+    
     public function editNote($id)
     {
         $id = Operations::decryptId($id);
-        echo "I'm editing note with id = {$id}";
+        //echo "I'm editing note with id = {$id}";
+
+        // load note
+        $note = Note::find($id);
+
+        // show edit note view
+        return view('edit_note', ['note' => $note]);       
     }
 
     public function deleteNote($id)
